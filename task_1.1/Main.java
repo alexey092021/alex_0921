7c74bd32c95a8ead82750b3d6b03e3af65f1766f
alexey092021 <90194315+alexey092021@users.noreply.github.com>
2021-09-07 17:21:42 +0300

    Update Main.java

--- task_1.1/Main.java
+++ task_1.1/Main.java
@@ -1,1 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String name = "J";
+        System.out.println(name);
+        int age = 20;
+         System.out.println(age);
+        String city = "L";
+        System.out.println(city);
 
+    }
+}
