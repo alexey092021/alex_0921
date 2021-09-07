044bc7f1eb0c08687e47a6495a65ad2f4397f2aa
alexey092021 <90194315+alexey092021@users.noreply.github.com>
2021-09-07 17:54:01 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("температура в градусах Цельсия: ");
+        float num1 = scanner.nextInt();
+        System.out.println("температура по Фаренгейту: " + (32+(9 * 
+
+num1) / 5));
+    }
+}
