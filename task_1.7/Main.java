761c7a063765c66ab77de3c7a5b536528697c769
alexey092021 <90194315+alexey092021@users.noreply.github.com>
2021-09-07 17:56:24 +0300

    Update Main.java

--- task_1.7/Main.java
+++ task_1.7/Main.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("длина: ");
+        int num1 = scanner.nextInt();
+        System.out.println("ширина: ");
+        int num2 = scanner.nextInt();
+        System.out.println("глубина: ");
+        int num3 = scanner.nextInt();
+        System.out.println(num1 * num2 * num3 );
+    }
 
+}
