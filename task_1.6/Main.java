9f2c3f2f99a85f710c23417ad34d75b2e5630626
alexey092021 <90194315+alexey092021@users.noreply.github.com>
2021-09-07 17:55:27 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите число: ");
+            int num1 = scanner.nextInt();
+            System.out.println(num1 * 1.15 );
 
+        }
+
+    }
