1829eac205679452448525ccfd6ab9411c5ebbd8
alexey092021 <90194315+alexey092021@users.noreply.github.com>
2021-09-07 17:50:21 +0300

    Update Main.java

--- task_1.3/Main.java
+++ task_1.3/Main.java
@@ -1,1 +1,11 @@
+public class Main {
 
+
+    public static void main(String[] args) {
+
+        String s = "book";
+        int a = 1;
+        int b = 2;
+        System.out.println(s+a+b);
+    }
+}
